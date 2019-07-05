6943dbc7f9c7615d6639498dec7925fb43c16846
Rodrigo <rodrigo.molina@labsit.io>
2019-07-05 16:44:23 -0300

    Aula 06 - 02 - navegando com history api

--- src/index.js
+++ src/index.js
@@ -2,13 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import AutorBox from './Autor';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 ReactDOM.render(
 <BrowserRouter>
     <Switch>
         <Route exact path="/" component={App} />
-        <Route path="/autor" />
+        <Route path="/autor" component={AutorBox} />
         <Route path="/livro" />
     </Switch>
 </BrowserRouter>,
