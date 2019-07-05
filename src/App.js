ca51f167cdfc7bed13cc653c79ee68247e344b01
Rodrigo <rodrigo.molina@labsit.io>
2019-07-04 21:15:36 -0300

    Aula 05 - 01 - refatorar o cadastro de autor

--- src/App.js
+++ src/App.js
@@ -1,59 +1,9 @@
 import React, { Component } from 'react';
 import './css/pure-min.css';
 import './css/side-menu.css';
-import $ from 'jquery';
-import InputCustomizado from './componentes/InputCustomizado';
+import { FormularioAutor, TabelaAutores } from './Autor';
 
 class App extends Component {
-
-    constructor() {
-        super();
-        this.state = { lista: [], nome:'', email:'', senha:'' };
-        this.enviaForm = this.enviaForm.bind(this);
-        this.setNome = this.setNome.bind(this);
-        this.setEmail = this.setEmail.bind(this);
-        this.setSenha = this.setSenha.bind(this);
-    }
-
-    componentDidMount() {
-        $.ajax({
-            url: "http://5d1e76083374890014f00d8c.mockapi.io/autores",
-            dataType: 'json',
-            success: function(resposta) {
-                this.setState({ lista: resposta });
-            }.bind(this)
-        });
-    }
-
-    enviaForm(evento) {
-        evento.preventDefault();
-
-        $.ajax({
-            url: "http://5d1e76083374890014f00d8c.mockapi.io/autores",
-            contentType: 'application/json',
-            dataType: 'json',
-            type: 'POST',
-            data: JSON.stringify({nome:this.state.nome, email:this.state.email, senha:this.state.senha}),
-            success: function(resposta) {
-                this.setState({lista: [...this.state.lista, resposta]});
-            }.bind(this), 
-            error: function(resposta) {
-                console.log('erro');
-            }
-        });
-    }
-
-    setNome(evento) {
-        this.setState({nome:evento.target.value})
-    }
-
-    setEmail(evento) {
-        this.setState({email:evento.target.value})
-    }
-
-    setSenha(evento) {
-        this.setState({senha:evento.target.value})
-    }
 
     render() {
       return (
@@ -83,40 +33,8 @@ class App extends Component {
                         <h1>Cadastro de Autores</h1>
                       </div>
                       <div className="content" id="content">
-                        <div className="pure-form pure-form-aligned">
-                          <form className="pure-form pure-form-aligned" onSubmit={this.enviaForm} method="POST">
-                            <InputCustomizado id="nome" type="text" name="nome" value={this.state.nome} onChange={this.setNome} label="Nome" />
-                            <InputCustomizado id="email" type="email" name="email" value={this.state.email} onChange={this.setEmail} label="Email" />
-                            <InputCustomizado id="senha" type="password" name="senha" value={this.state.senha} onChange={this.setSenha} label="Senha" />
-                            <div className="pure-control-group">                                  
-                              <label></label> 
-                              <button type="submit" className="pure-button pure-button-primary">Gravar</button>                                    
-                            </div>
-                          </form>             
-
-                        </div>  
-                        <div>            
-                          <table className="pure-table">
-                            <thead>
-                              <tr>
-                                <th>Nome</th>
-                                <th>email</th>
-                              </tr>
-                            </thead>
-                            <tbody>
-                              {
-                                  this.state.lista.map(function(autor){
-                                      return (
-                                          <tr key={autor.id}>
-                                              <td>{autor.nome}</td>
-                                              <td>{autor.email}</td>
-                                          </tr>
-                                      );
-                                  })
-                              }
-                            </tbody>
-                          </table> 
-                        </div>             
+                        <FormularioAutor />  
+                        <TabelaAutores />           
                       </div>
                     </div>            
                   </div>     
